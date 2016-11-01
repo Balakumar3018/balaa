05bc8b6d21809f57361279888dbd994f0203af17
Balakumarlakshmi <balakumarjkp@gmail.com>
2016-11-01 20:05:55 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,pow;
+    printf("ente a number");
+    scanf("%d",&num);
+    pow=num*num;
+    printf("%d",pow);
+    return 0;
+}
